29ac7ca64dfd5784c8d89ace6540590de1985178
cindyhanae <cinde01@gmail.com>
2021-05-13 15:04:33 -0300

    slider definitivo

--- script.js
+++ script.js
@@ -4,3 +4,97 @@ $(function() {
         //your configuration goes here
     });
 });
+
+
+
+// SLIDERS
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function plusSlides2(m) {
+    showSlides2(slideIndex2 += m);
+  }
+
+  function currentSlide2(m) {
+    showSlides2(slideIndex2 = m);
+  }
+
+  function plusSlides3(o) {
+    showSlides3(slideIndex3 += o);
+  }
+
+  function currentSlide3(o) {
+    showSlides3(slideIndex3 = o);
+  }
+
+//   SLIDER 1
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+      slideIndex = 1
+    };
+    if (n < 1) {
+      slideIndex = slides.length
+    };
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    };
+    slides[slideIndex - 1].style.display = "block";
+  };
+
+//   SLIDER 2
+  function showSlides2(m) {
+    var j;
+    var slides2 = document.getElementsByClassName("mySlides2");
+    if (m > slides2.length) {
+      slideIndex2 = 1
+    };
+    if (m < 1) {
+      slideIndex2 = slides2.length
+    };
+    for (j = 0; j < slides2.length; j++) {
+      slides2[j].style.display = "none";
+    };
+    slides2[slideIndex2 - 1].style.display = "block";
+  };
+
+//   SLIDER 3
+  function showSlides3(o) {
+    var l;
+    var slides3 = document.getElementsByClassName("mySlides3");
+    if (o > slides3.length) {
+      slideIndex3 = 1
+    };
+    if (o < 1) {
+      slideIndex3 = slides3.length
+    };
+    for (l = 0; l < slides3.length; l++) {
+      slides3[l].style.display = "none";
+    };
+    slides3[slideIndex3 - 1].style.display = "block";
+  };
+
+  var slideIndex = 1;
+  var slideIndex2 = 1;
+  var slideIndex3 = 1;
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("first DOMContentLoaded event");
+    showSlides(slideIndex);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("second DOMContentLoaded event");
+    showSlides2(slideIndex2);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("third DOMContentLoaded event");
+    showSlides3(slideIndex3);
+  });
